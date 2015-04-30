654ddc6f2a8fe18cd5cc0a727ba9ca6295bab714
xpch0902 <6247868@qq.com>
2015-04-30 12:53:15 +0800

    Update triangleTest.js

--- triangle/triangleTest.js
+++ triangle/triangleTest.js
@@ -18,4 +18,4 @@ QUnit.test("scalene triangle has three different sides", function( assert ) {
 	assert.equal(triangle.isScalene(3,2,4),true,"3,2,4 is scalene") ;
 	assert.equal(triangle.isScalene(1,2,6),false,"1,2,6 is scalene") ;
 	
-});//BIPT-09
+});
